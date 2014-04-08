3b53724d3c5215eb346b2ccbbee7e455ad2c7a1c
Julio Arias <jarias01@gmail.com>
2014-04-08 16:37:23 -0600

    Complete the stormpath user repository

--- app/templates/src/main/java/package/domain/_User.java
+++ app/templates/src/main/java/package/domain/_User.java
@@ -3,9 +3,12 @@ package <%=packageName%>.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import <%=packageName%>.domain.util.EntityWrapper;
 import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.group.Group;
 import lombok.Data;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -15,6 +18,11 @@ public class User implements Resource<String> {
     private String id;
     private String name;
     private String email;
+    private String password;
+    private List<String> groups = new ArrayList<>();
+
+    public User() {
+    }
 
     public User(String username, String name, String email) {
         this.id = username;
@@ -26,6 +34,10 @@ public class User implements Resource<String> {
         this.id = account.getUsername();
         this.name = account.getFullName();
         this.email = account.getEmail();
+
+        for (Group group : account.getGroups()) {
+            groups.add(group.getName());
+        }
     }
 
     @Data
